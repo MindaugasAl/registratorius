ce02de6395c562b242bc66e7163948b3804e7278
MindaugasAl <minals99@gmail.com>
2022-09-16 02:41:08 +0300

    registratorius

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express'
-import { engine } from 'express-handlebars';
+import session from 'express-session'
+import { engine } from 'express-handlebars'
 import fs from 'fs/promises'
+import auth from './middleware/auth.js'
 
 const app = express();
 const file = './duomenys.json'
@@ -9,6 +11,14 @@ const file = './duomenys.json'
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
+app.use(session({
+    secret: 'labai smarkiai slapta fraze',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { 
+                maxAge: 60000
+    }
+  }))
 
 app.use('/public', express.static('public'))
 app.use(express.urlencoded({
@@ -34,6 +44,8 @@ app.post('/', async (req, res) => {
         const data = await fs.readFile(file, 'utf8')
         if (!JSON.parse(data).find(user => user.email === req.body.email && user.password === req.body.password))
             return res.render('login', { message: 'Neteisingi prisijungimo duomenys', status: 'danger' })
+
+        req.session.loggedIn = true
 
         return res.redirect('/admin')
     } catch {
@@ -74,9 +86,10 @@ app.post('/register', async (req, res) =>{
     }
 })
 
-app.get('/admin', async (req, res) => {
 
-    const data = await fs.readFile(file, 'utf8')
+
+app.get('/admin', auth, async (req, res) => {
+        const data = await fs.readFile(file, 'utf8')
     const users = JSON.parse(data)
     const options = { users }
     if (req.query.success == 1){
@@ -90,7 +103,10 @@ app.get('/admin', async (req, res) => {
     res.render('admin',  options)
 })
 
-app.get('/delete/:id', async (req, res) => {
+app.get('/delete/:id', auth ,async (req, res) => {
+    // if(!req.session.loggedIn)
+    // return res.redirect('/')
+    
     try {
         const data = await fs.readFile(file, 'utf8')
         let users = JSON.parse(data)
