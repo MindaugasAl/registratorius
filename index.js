6bc7b61ba31cc8dd42988be330e68fd93f57871f
MindaugasAl <minals99@gmail.com>
2022-09-15 09:35:18 +0300

    registratorius

--- index.js
+++ index.js
@@ -24,24 +24,21 @@ app.get('/', (req, res) => {
     res.render('login', options);
 });
 
-app.get('/admin', (req, res)=>{
-    res.render('admin')
-})
 
 app.post('/', async (req, res)=> {
     if (req.body.email === '' || req.body.password === '')
-return res.render('login', {message: 'Neįvesti prisijungimo duomenys', status: 'danger'})
-
-try{
-const data = await fs.readFile(file, 'utf8')
-if (!JSON.parse(data).find(user => user.email === req.body.email && user.password === req.body.password)) 
-     return res.render('login', {message: 'Neteisingi prisijungimo duomenys', status: 'danger'})
-
-     return res.redirect('/admin')
-}catch{
-    return res.render('login', {message: 'Duomenų bazės failas nerastas', status: 'danger'})
-}
-
+    return res.render('login', {message: 'Neįvesti prisijungimo duomenys', status: 'danger'})
+    
+    try{
+        const data = await fs.readFile(file, 'utf8')
+        if (!JSON.parse(data).find(user => user.email === req.body.email && user.password === req.body.password)) 
+        return res.render('login', {message: 'Neteisingi prisijungimo duomenys', status: 'danger'})
+        
+        return res.redirect('/admin')
+    }catch{
+        return res.render('login', {message: 'Duomenų bazės failas nerastas', status: 'danger'})
+    }
+    
 })
 
 app.get('/register', async (req, res) => {
@@ -50,26 +47,34 @@ app.get('/register', async (req, res) => {
         req.query.name !== '' &&
         req.query.email !== '' &&
         req.query.password !== '') {
-
-        try {
-            let data = await fs.readFile(file, 'utf8');
-            data = JSON.parse(data)
-            if (data.find(user => user.email === req.query.email)) {
-                return res.render('register', { message: 'Toks el., paštas jau registruotas', status: 'danger' })
-            }
-            data.push(req.query)
-            await fs.writeFile(file, JSON.stringify(data, null, 4))
-            // return res.render('register', { message: 'Vartotojas sėkmingai užregistruotas <a href="/">prisijunkite</a>', status: 'success' })
+            
+            try {
+                let data = await fs.readFile(file, 'utf8');
+                data = JSON.parse(data)
+                if (data.find(user => user.email === req.query.email)) {
+                    return res.render('register', { message: 'Toks el., paštas jau registruotas', status: 'danger' })
+                }
+                data.push(req.query)
+                await fs.writeFile(file, JSON.stringify(data, null, 4))
+                // return res.render('register', { message: 'Vartotojas sėkmingai užregistruotas <a href="/">prisijunkite</a>', status: 'success' })
             return res.redirect("/?status=1")
         } catch {
             await fs.writeFile(file, JSON.stringify([req.query], null, 4))
         }
-
+        
     } else {
         return res.render('register', { message: 'Prašome užsiregitruoti', status: 'danger' })
-
+        
     }
     res.render('register')
+})
+
+app.get('/admin', async (req, res)=>{
+
+    const data = await fs.readFile(file, 'utf8')
+    const users = JSON.parse(data)
+
+    res.render('admin', {users})
 })
 
 
